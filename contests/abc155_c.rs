12870eadbb1f40366f435e0c18ca10c77950fcd5
Lttce <31960436+Lttce@users.noreply.github.com>
2023-04-07 14:09:01 +0000

    update:

--- contests/abc155_c.rs
+++ contests/abc155_c.rs
@@ -10,18 +10,15 @@ fn main() {
         s: [String; n],
     }
 
-    let mut m = std::collections::HashMap::<String, i32>::new();
+    let mut m = std::collections::BTreeMap::<String, i32>::new();
     for ss in s {
         *m.entry(ss).or_insert(0) += 1
     }
 
     let max = &m.values().max().unwrap();
-    let mut ans = vec![];
-    for mm in &m {
-        if mm.1 == *max {
-            ans.push(mm.0.clone())
+    for (k, v) in &m {
+        if v == *max {
+            println!("{}", k);
         }
     }
-    ans.sort();
-    println!("{}", ans.join("\n"));
 }
